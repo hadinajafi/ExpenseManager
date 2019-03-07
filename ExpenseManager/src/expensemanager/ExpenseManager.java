61ef4f0f18e9ded4eb955579642c089676399721
hadinajfi77 <hadinajafi.77@gmail.com>
2019-03-07 22:16:49 +0330

    initialize main class

---
+++ ExpenseManager/src/expensemanager/ExpenseManager.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expensemanager;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author hadin
+ */
+public class ExpenseManager extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            AnchorPane root = FXMLLoader.load(getClass().getResource("MainLayout.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("mainlayout.css").toExternalForm());
+            primaryStage.setTitle("Expense Manager");
+            primaryStage.setScene(scene);
+            primaryStage.setMinWidth(700);
+            primaryStage.setMinHeight(466);
+            primaryStage.show();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(ExpenseManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
