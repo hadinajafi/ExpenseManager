e4ac6627891daad09ffbbf89d7018574d8f055ba
hadinajfi77 <hadinajafi.77@gmail.com>
2019-04-21 22:39:34 +0430

    initialize new transaction window & weekly view

--- ExpenseManager/src/expensemanager/MainLayoutController.java
+++ ExpenseManager/src/expensemanager/MainLayoutController.java
@@ -8,14 +8,18 @@ package expensemanager;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TreeTableView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -35,6 +39,16 @@ public class MainLayoutController implements Initializable {
     @FXML
     private Button addNewAccountBtn;
     @FXML
+    private Label statusLabel;
+    @FXML
+    private Button addNewActivityBtn;
+    @FXML
+    private Label todayDateLabel;
+    @FXML
+    private TreeTableView<?> weeklyViewTreeTable;
+    
+    //Actions for componetns
+    @FXML
     void addNewAccountBtnAction(ActionEvent event) {
         AnchorPane root = null;
         try {
@@ -47,13 +61,30 @@ public class MainLayoutController implements Initializable {
         //loading the css file
         //scene.getStylesheets().add(getClass().getResource("addnewtasklayout.css").toExternalForm());
         Stage stage = new Stage();
-        stage.setTitle("Add New Task");
+        stage.setTitle("Add New Account");
         stage.setResizable(false);  //setting resizable to false.
         stage.setMinWidth(566);
         stage.setMinHeight(353);
         stage.setScene(scene);
         stage.show();
     }
+    @FXML
+    void addNewActivityBtnAction(ActionEvent event) {
+        AnchorPane root = null;
+        try{
+            root = FXMLLoader.load(getClass().getResource("AddNewTransactionLayout.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setTitle("Add New Transaction");
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(MainLayoutController.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println(ex.getMessage());
+        }
+    }
+    
     /**
      * Initializes the controller class.
      * @param url
